a707afde4cac1b2135fa28c2dede8f7db1f59363
Francesco Poggi <fpoggi@cs.unibo.it>
2019-02-26 11:51:59 +0100

    Updated .gitignore and run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ java -jar target/asn-analysis-1.0-jar-with-dependencies.jar -i data/input/datase
 java -jar target/asn-analysis-1.0-jar-with-dependencies.jar -i data/input/dataset-ASN-2012.arff -o data/output/Areas-Level2.csv -op Area -l 2
 java -jar target/asn-analysis-1.0-jar-with-dependencies.jar -i data/input/dataset-ASN-2012.arff -o data/output/FeatureSel-Level1.csv -op FeatureSel -l 1
 java -jar target/asn-analysis-1.0-jar-with-dependencies.jar -i data/input/dataset-ASN-2012.arff -o data/output/FeatureSel-Level2.csv -op FeatureSel -l 2
-java -jar target/asn-analysis-1.0-jar-with-dependencies.jar -i data/input/experiment1/ -o data/output/reordered/Experiment1.csv -op Experiment1
-java -jar target/asn-analysis-1.0-jar-with-dependencies.jar -i data/input/experiment2/ -o data/output/reordered/Experiment2.csv -op Experiment2
+java -jar target/asn-analysis-1.0-jar-with-dependencies.jar -i data/input/experiment1/ -o data/output/Experiment1.csv -op Experiment1
+java -jar target/asn-analysis-1.0-jar-with-dependencies.jar -i data/input/experiment2/ -o data/output/Experiment2.csv -op Experiment2
